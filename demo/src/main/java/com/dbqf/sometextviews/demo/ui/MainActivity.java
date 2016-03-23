accb4aa34b6a542aee1231785d3976932555acc3
dubuqingfeng <1135326346@qq.com>
2016-03-23 19:47:37 +0800

    init project

---
+++ demo/src/main/java/com/dbqf/sometextviews/demo/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dbqf.sometextviews.demo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.dbqf.sometextviews.demo.R;
+
+/**
+ * Created by qingfeng on 16/3/23.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
